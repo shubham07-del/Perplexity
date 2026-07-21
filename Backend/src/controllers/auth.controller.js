c71f28e727c96988a25e71e7731f43ad8a353547
Shubham Lenka <shubhamlenka43@gmail.com>
2026-07-21 15:37:42 +0530

    fix the email related issue

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export async function register(req,res){
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="https://perplexity-liard.vercel.app/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="https://perplexity-s7gf.onrender.com/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
