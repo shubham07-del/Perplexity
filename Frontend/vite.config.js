6b311ba12c9de55cc9f6e91e9fe186b82acccf80
Shubham Lenka <shubhamlenka43@gmail.com>
2026-07-23 17:01:19 +0530

    handling the google auth error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin-allow-popups",
+      "Cross-Origin-Opener-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "unsafe-none",
     },
   },
 })
